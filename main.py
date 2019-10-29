479d3864a2e8ac814a426091acfea3c50bce9585
sergeyrid0 <sergeyrid@yandex.ru>
2019-10-29 12:48:07 +0300

    main

--- main.py
+++ main.py
@@ -6,18 +6,20 @@ from Icebergs import spawn
 pygame.init()
 width, height = 10, 3
 side, thickness = 100, 1
-xres, yres = width * side, height * side
+margin = side
+xres, yres = width * side + 2 * margin, height * side + 2 * margin
 screen = pygame.display.set_mode((xres, yres))
-screenarr = pygame.PixelArray(screen)
 pygame.draw.rect(screen, (255, 255, 255), (0, 0, xres, yres))
 
 def draw_field():
     for i in range(width):
         for j in range(height):
-            pygame.draw.rect(screen, (0, 0, 0), (i * side, j * side, side, side), 1)
+            pygame.draw.rect(screen, (0, 0, 0), (i * side + margin, j * side + margin, side, side), 1)
 
+def draw_ship(x, y):
+    pygame.draw.rect(screen, (0, 255, 0), (x * side + margin, y * side + margin, 10, 10)
 
-spawn(width, height, screen, side)
+spawn(width, height, screen, side, margin)
 draw_field()
 #draw_ship()
 pygame.display.flip()
