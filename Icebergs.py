9ed89fa0d322671536a0a9aa186a6d7d5843a29b
serchermach <serchemach@gmail.com>
2019-10-29 12:45:20 +0300

    Icebergs

--- Icebergs.py
+++ Icebergs.py
@@ -2,9 +2,9 @@ from random import randrange
 import pygame
 
 
-def spawn(n, m, screen, size):
+def spawn(n, m, screen, size, shift):
     iceberg = pygame.image.load('iceberg.png')
-    iceberg = pygame.transform.scale(iceberg, (size // 3, size // 3))
+    iceberg = pygame.transform.scale(iceberg, (size // 2, size // 2))
     x1, x2 = randrange(n), randrange(n)
     y1, y2 = randrange(m), randrange(m)
     if x1 == x2 and y1 == y2:
@@ -12,6 +12,6 @@ def spawn(n, m, screen, size):
             x1 -= 1
         else:
             x1 += 1
-    screen.blit(iceberg, (x1 * size, y1 * size + size // 3))
-    screen.blit(iceberg, (x2 * size, y2 * size + size // 3))
+    screen.blit(iceberg, (x1 * size + shift, y1 * size + size // 4 + shift))
+    screen.blit(iceberg, (x2 * size + shift, y2 * size + size // 4 + shift))
     return x1, y1, x2, y2
