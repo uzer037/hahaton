63e57dbef12cddf24768417841841b03e4f0646f
serchemach <serchemach@gmail.com>
2019-10-30 03:36:45 +0300

    Path lines fixed

--- Story.py
+++ Story.py
@@ -1,6 +1,16 @@
 import pygame
 
 
+def render_text(screen, arr, myfont, xres, yres):
+    for i in range(len(arr)):
+        for j in range(len(arr[i])):
+            pygame.event.get()
+            screen.blit(myfont.render(arr[i][:j + 1], False, (255, 255, 255)), (xres // 24, yres // 24 * (i + 1)))
+            pygame.display.flip()
+            pygame.time.wait(100)
+        pygame.time.wait(100)
+
+
 def play(screen, xres, yres):
     pygame.init()
     myfont = pygame.font.SysFont('Comic Sans MS', xres // 24)
@@ -20,20 +30,20 @@ def play(screen, xres, yres):
            "to put and end to this.",
            "Are you prepared to meet things that will",
            "lie there?"]
-    for i in range(len(arr)):
-        for j in range(len(arr[i])):
-            pygame.event.get()
-            screen.blit(myfont.render(arr[i][:j + 1], False, (255, 255, 255)), (xres // 24, xres // 24 * (i + 1)))
-            pygame.display.flip()
-            pygame.time.wait(100)
-        pygame.time.wait(100)
-
-    done = False
-    while not done:
-        for event in pygame.event.get():
-            if event.type == pygame.KEYDOWN:
-                if event.key == pygame.K_ESCAPE:
-                    done = True
+    render_text(screen, arr, myfont, xres, yres)
 
 
-# play(pygame.display.set_mode((500, 500)), 500, 500)
+def victory(screen, xres, yres):
+    myfont = pygame.font.SysFont('Comic Sans MS', xres // 24)
+    arr = ["You won, congratulations."]
+    render_text(screen, arr, myfont, xres, yres)
+
+
+def lose(screen, xres, yres):
+    myfont = pygame.font.SysFont('Comic Sans MS', xres // 24)
+    arr = ["You were unable to solve",
+           "the mysteries that lie beyond."]
+    render_text(screen, arr, myfont, xres, yres)
+
+
+play(pygame.display.set_mode((500, 500)), 500, 500)
